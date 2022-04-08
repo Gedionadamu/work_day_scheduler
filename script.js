4f143878d094648a9003c3e5b74ff331622fc9a9
Gedion Adamu <gedionadamu@gmail.com>
2022-04-07 23:26:29 -0400

    reworked user interface and local storage

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM, Do"));
 
-var userInput = $(".descritpion")
-console.log(userInput)
+var hour = moment().hour(); // Number
+
+
+
+
+var saveButton = $(".saveBtn")
+
+
+saveButton.on('click',saveUserInput);
+
+
 
 function saveUserInput(){
-    var descritpion = { dayEvent : descritpion.value};
+    var userInput = $(this).siblings('.description').val();
+    var time = $(this).attr("id");
+    var descritpion = { userInput,time};
     localStorage.setItem("description", JSON.stringify(descritpion));
 };
-// function renderUserInput(){
-//     var lastInput = JSON.parse(localStorage.getItem("descritpion"));
-//     if (descritpion !== null) {
-//         userInput.
+function renderUserInput(){
+    var lastInput = JSON.parse(localStorage.getItem("descritpion"));
+    
+    // if (lastInput !== null) {
+    //     userInput
 
-//     }
-// }
+    // }
+}
+console.log(renderUserInput)
