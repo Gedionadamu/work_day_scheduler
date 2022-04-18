7eb831568f3df53f326ea663846f1ec8e22a4fa5
Gedion Adamu <gedionadamu@gmail.com>
2022-04-17 21:45:28 -0400

    updated readme file and commenting with some code rework

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # work_day_scheduler
+
+## User Story 
+
+An employee with a busy schedule wants to add important events to a daily planner so they can manage my time effectively. 
+
+## Acceptance Criteria
+
+```
+When I opening the planner the current day is displayed at the top of the calendar, and user scrolls down they are presented with timeblocks for standard business hours.
+When viewing the timeblocks for that day each timeblock is color coded to indicate whether it is in the past, present, or future
+When user clicks into a timeblock, user can enter an event and click save button to save to local sotrage. 
+When user refreshs the page the saved events persist
+
+```
+
+### Screenshot 
+
+![ screen shot for work day scheduler](./Screenshot.jpg)
+
+### Github deployed page
+
+https://gedionadamu.github.io/work_day_scheduler
--- script.js
+++ script.js
@@ -1,71 +1,62 @@
+// creates variable and appends time to the top of page
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM, Do"));
-
+ // creates a variable the gives the current hour in the 24hr format
 var hour = moment().hour(); // Number
 
 
 
 
+// creates a variable for the the save btn
 var saveButton = $(".saveBtn")
 
-
+//  creates an event listner on the search btn
 saveButton.on('click',saveUserInput);
-
-timeManager();
-// renderUserInput();
+// creates an empty array variable
 var todo = [];
-
+// creates a function to save unser input into local storage
 function saveUserInput(){
-    renderUserInput();
-    var userInput = $(this).siblings('.description').val();
-    var time = $(this).attr("id");
+    
+    //  creates a loop to save values in every input feild
+    for (var i = 0; i<9;i++){
+    var userInput = $("#time" + [i]).val();
+    var time = $(".time"+ [i]).attr("id");
     var description = { userInput,time};
+    // adds items to an empty array
     todo.push(description);
+}
+    // saves to local storage
     localStorage.setItem("description", JSON.stringify(todo));
     
 };
-function renderUserInput(){
-    // var lastInput = JSON.parse(localStorage.getItem("description"));
-    // var time = lastInput.time;
-    // var dis = lastInput.userInput;
-    // if(time === $(".description").prop("id")){
-    //     $(this).text= dis;
-    // console.log(time)
-    // console.log(dis)   
 
+// function renders data from local storage to html
+function renderstorage(){
+    // creates a variable  and gets items from local storage
+    var localsave = JSON.parse(localStorage.getItem("description"));
+    // it tells the function to stop if there is nothing in the local storage
+    if (localsave == null){
+        return
+    }
+    // loops through each input feild and puts the local storage data in the input values
+    for(var i= 0; i < 9; i++){
+               
+        $("#time"+[i]).val(localsave[i].userInput)
+        // loops through the time and shows the correct color for the time of day. 
+        if (localsave[i].time < hour){
+            $("#time"+[i]).attr("class", "past col-10")
+        }
+        else if (localsave[i].time == hour){
+            $("#time"+[i]).attr("class", "present col-10")
+        }
+        else {
+            $("time"+[i]).attr("class", "future col-10")
+        }
+        // console.log(localsave[i].time)
     }
     
-
-
- function timeManager(){
-    var des = $(".description")
-    var scheduleTime = $(".description").prop("id");
     
-    for(i=0; i<hour;i)
-        if(hour == scheduleTime){
-            des.attr("class","present col-10")
-        } else if ( hour > scheduleTime){
-            des.attr("class", "future col-10")
-        } else{
-            des.attr("class","past col-10")
-        }
+}
 
- }
-// // function saveUserInput(){
-// //     var userInput = $(this).siblings('.description').val();
-// //     var time = $(this).attr("id");
-// //     var descritpion = { userInput,time};
-// //     localStorage.setItem("description", userInput);
-// //     localStorage.setItem("time",time);
-// //     renderUserInput();
-// // };
-// // function renderUserInput(){
-// //     var lastInput = localStorage.getItem("description");
-// //     var lasttime = localStorage.getItem("time");
-// //     // if (lastInput !== null) {
-// //     //     userInput
-
-// //     // }
-// //     console.log(lasttime);
-// //     console.log(lastInput);
-// // }
+// calls the renderstorage function. 
+renderstorage();
